e782aa1b1d7fb54bebe87c890f56ff391bea0e14
THIKKAVARAPU MANIKANTA REDDY <t.manikantareddy160@gmail.com>
2019-07-25 02:34:17 -0700

    SecondCommit

--- index.js
+++ index.js
@@ -17,6 +17,43 @@ app.get('/',(req,res)=>{
     })
 })
 
+var store
+//@create new datastore
+app.get('/:filename',(req,res)=>{
+    store=new datastore({path:`./data-store/${req.params.filename}.json`})
+    // res.json(`Currently ,You are in ${req.params.filename} file`)
+    res.json(JSON.parse(store.json(null,2)))
+})
+
+//@post/:filename/create
+app.post('/:filename/update',(req,res)=>{
+    
+    store=new datastore({path:`./data-store/${req.params.filename}.json`})
+    
+    Object.entries(req.body).forEach(entry=>{
+        if(!store.hasOwn(entry[0])){
+            
+          store.set(entry[0],entry[1])
+        }      
+    })
+    res.json(JSON.parse(store.json(null,2))) 
+})
+
+//@get/:filename/getvalue
+
+app.get('/:filename/getvalue',(req,res)=>{
+
+    store=new datastore({path:`./data-store/${req.params.filename}.json`}) 
+    var array=[]
+    Object.entries(req.body).forEach(entry=>{
+        if(store.hasOwn(entry[0])){
+            
+         array.push(store.get(entry[0]))
+        }       
+    })
+    res.json({Values :array})
+})
+
 app.listen(3000,()=>{
     console.log('app is running at '+port)    
 })
